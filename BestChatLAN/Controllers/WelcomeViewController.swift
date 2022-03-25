09f06e56ccff956882ce133d0ab599bf70597cad
Isaac Sanchez <isaacsanchez@mac-kun.local>
2022-03-24 18:14:17 -0700

    Pod added and Unit test bundle created

--- BestChatLAN/Controllers/WelcomeViewController.swift
+++ BestChatLAN/Controllers/WelcomeViewController.swift
@@ -6,14 +6,22 @@
 //
 
 import UIKit
+import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        titleLabel.charInterval = 0.1
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        titleLabel.text = "🚀 BestChatLAN 🚀"
     }
 
 }
---
+++ Pods/Target Support Files/Pods-BestChatLAN/Pods-BestChatLAN-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BestChatLAN : NSObject
+@end
+@implementation PodsDummy_Pods_BestChatLAN
+@end
